58736eeede3d888edef4cb330d307c4555f8f1ea
Elena Guzmán <91577901+strxngers@users.noreply.github.com>
2023-06-14 23:31:08 -0400

    [Ivania Elena Guzmán] pregunta 1 control TBD

---
+++ Respuestas control 2.sql
@@ -0,0 +1,25 @@
+-- Desarrollo control 2 TBD
+-- Integrantes Grupo 1: Elena Guzmán, Paolo Demarchi, Ignacio Moreira, Fernando Pérez, Gaspar Catalán.
+
+-- Pregunta 1:
+-- Se le agrega a la tabla minimarkets_gs una columna en la que se tienen las ubicaciones como punto
+ALTER TABLE minimarkets_gs ADD COLUMN geom GEOMETRY;
+UPDATE minimarkets_gs SET geom = ST_SetSRID(ST_MakePoint(longitud,latitud),4326);
+
+-- Se elimina la tabla si existe una con este nombre anteriormente
+DROP TABLE IF EXISTS zc_500m;
+
+-- Creación de tabla con todas las zonas censales que se encuentren a menos de 500 metros de algún minimarket
+CREATE TABLE zc_500m AS(
+	SELECT zona.id, zona.comuna, zona.nom_comuna, zona.geom, zona.cod_distri, zona.cod_zona, zona.geocodigo, zona.shape_leng, zona.shape_area
+	FROM zonas_censales_gs AS zona
+	LEFT JOIN minimarkets_gs mini ON ST_DWithin(ST_Transform(mini.geom, 32719), zona.geom, 500)
+	WHERE mini.nombre IS NULL);
+	
+	
+	
+-- Pregunta 2:
+
+-- Se elimina la tabla si existe una con este nombre anteriormente
+DROP TABLE IF EXISTS comunas_ptje_mini;
+
